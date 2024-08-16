ff72d9253cbc09e6bc39102bf9c9538e85e594d9
anyel-ec <paulangel826@gmail.com>
2024-08-16 17:33:21 -0500

    start project in spring boot v3.3.2

---
+++ src/main/java/top/anyel/jwr/SpringbootSecurityJwtRolesApplication.java
@@ -0,0 +1,13 @@
+package top.anyel.jwr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityJwtRolesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSecurityJwtRolesApplication.class, args);
+    }
+
+}
